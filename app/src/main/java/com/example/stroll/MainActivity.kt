6ff401ccdb8cea91d87ad6f6ef45392fb69665e3
Josh <onabanjotobi@gmail.com>
2020-01-02 20:56:31 +0000

    Added some fragments to navigate to via bottom navigation bottom bar

--- app/src/main/java/com/example/stroll/MainActivity.kt
+++ app/src/main/java/com/example/stroll/MainActivity.kt
@@ -3,21 +3,39 @@ package com.example.stroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.luseen.spacenavigation.SpaceItem
 import com.luseen.spacenavigation.SpaceNavigationView
 import com.luseen.spacenavigation.SpaceOnClickListener
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val CENTRE_BUTTON_INDEX = -1
+const val CURRENT_SELECTED_ITEM_BUNDLE_KEY = "currentItem"
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var navigationView: SpaceNavigationView
 
+    private val firstFragment by lazy { FirstFragment.newInstance() }
+    private val secondFragment by lazy { SecondFragment.newInstance() }
+    private val profileFragment by lazy { ProfileFragment.newInstance() }
+
+    private var currentSelectedItem = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         navigationView = findViewById(R.id.bottomNavigationView)
 
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(CURRENT_SELECTED_ITEM_BUNDLE_KEY))
+                currentSelectedItem = savedInstanceState.getInt(CURRENT_SELECTED_ITEM_BUNDLE_KEY, 0)
+        }
+
         initBottomNavigationView(savedInstanceState)
+        initFragment()
     }
 
     private fun initBottomNavigationView(savedInstanceState: Bundle?) {
@@ -29,24 +47,75 @@ class MainActivity : AppCompatActivity() {
         navigationView.initWithSaveInstanceState(savedInstanceState)
         navigationView.addSpaceItem(SpaceItem("", R.drawable.ic_history_black_24dp))
         navigationView.addSpaceItem(SpaceItem("", R.drawable.ic_add_black_24dp))
-        navigationView.addSpaceItem(SpaceItem("", R.drawable.ic_favorite_black_24dp))
-        navigationView.addSpaceItem(SpaceItem("", R.drawable.ic_settings_black_24dp))
     }
 
     private fun setNavigationViewClickListener() {
-        navigationView.setSpaceOnClickListener(object: SpaceOnClickListener {
+        navigationView.setSpaceOnClickListener(object : SpaceOnClickListener {
             override fun onCentreButtonClick() {
-                Toast.makeText(applicationContext, "onCentreButtonClicked", Toast.LENGTH_SHORT).show()
                 navigationView.setCentreButtonSelectable(true)
+                navigationView.setCentreButtonSelected()
+                setCurrentSelectedItem(CENTRE_BUTTON_INDEX)
+                setFragment(profileFragment)
             }
 
             override fun onItemReselected(itemIndex: Int, itemName: String?) {
-                Toast.makeText(applicationContext, "$itemIndex - $itemName", Toast.LENGTH_SHORT).show()
+
             }
 
             override fun onItemClick(itemIndex: Int, itemName: String?) {
-                Toast.makeText(applicationContext, "$itemIndex - $itemName", Toast.LENGTH_SHORT).show()
+                when (itemIndex) {
+                    0 -> {
+                        setCurrentSelectedItem(itemIndex)
+                        setFragment(firstFragment)
+                    }
+                    1 -> {
+                        setCurrentSelectedItem(itemIndex)
+                        setFragment(secondFragment)
+                    }
+                    else -> {
+                        Toast.makeText(applicationContext, "$itemIndex - $itemName Not implemented", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
             }
         })
+    }
+
+    /**
+     * Set a fragment when the activity is created.
+     */
+    private fun initFragment() {
+        when (currentSelectedItem) {
+            -1 -> {
+                setFragment(profileFragment)
+            }
+
+            0 -> {
+                setFragment(firstFragment)
+            }
+
+            1 -> {
+                setFragment(secondFragment)
+            }
+        }
+    }
+
+    private fun setCurrentSelectedItem(itemIndex: Int) {
+        currentSelectedItem = itemIndex
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.inTransaction {
+            replace(R.id.fragmentContainer, fragment)
+        }
+    }
+
+    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+        beginTransaction().func().commit()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        bottomNavigationView.onSaveInstanceState(outState)
     }
 }
